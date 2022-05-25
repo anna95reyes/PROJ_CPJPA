5f50165e7098932a555686dd2b6e53e621b6d7ee
Anna <areyes@milaifontanals.org>
2022-05-25 21:43:11 +0200

    25/05/2022

--- src/org/milaifontanals/jpa/CPJPA.java
+++ src/org/milaifontanals/jpa/CPJPA.java
@@ -9,14 +9,18 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import org.milaifontanals.interficie.GestioProjectesException;
 import org.milaifontanals.interficie.IGestioProjectes;
 import org.milaifontanals.model.Projecte;
+import org.milaifontanals.model.ProjecteUsuariRol;
+import org.milaifontanals.model.Rol;
 import org.milaifontanals.model.Usuari;
 
 /**
@@ -24,6 +28,13 @@ import org.milaifontanals.model.Usuari;
  * @author anna9
  */
 public class CPJPA implements IGestioProjectes {
+    
+    private HashMap<Integer, Projecte> hmProjectes = new HashMap();
+    private HashMap<Integer, Usuari> hmUsuaris = new HashMap();
+    private HashMap<Integer, Projecte> hmProjectesAssignats = new HashMap();
+    private HashMap<Integer, Projecte> hmProjectesNoAssignats = new HashMap();
+    private Rol rol;
+    
     private EntityManager em;
     
     public CPJPA() throws GestioProjectesException {
@@ -52,9 +63,7 @@ public class CPJPA implements IGestioProjectes {
         EntityManagerFactory emf = null;
         try {
             emf = Persistence.createEntityManagerFactory(up, props);
-//            System.out.println("EMF creat");
             em = emf.createEntityManager();
-//            System.out.println("EM creat");
         } catch (Exception ex) {
             if (emf != null) {
                 emf.close();
@@ -63,6 +72,160 @@ public class CPJPA implements IGestioProjectes {
         }
 
     }
+
+    @Override
+    public List<Usuari> getLlistaUsuaris() throws GestioProjectesException {
+        Query query = em.createQuery("select u from Usuari u", Usuari.class);
+        List<Usuari> usuaris = query.getResultList();
+        for (Usuari usu: usuaris) {
+            hmUsuaris.put(usu.getId(), usu);
+        }
+        return usuaris;
+    }
+    
+    @Override
+    public Usuari getUsuari(int id) throws GestioProjectesException {
+        if (id <= 0 || id > Integer.MAX_VALUE) {
+            throw new GestioProjectesException("Intent de cercar un usuari amb id " + id + " inadequat");
+        }
+        
+        Usuari usu = hmUsuaris.get(id);
+        if (usu != null){
+            return usu;
+        }
+        
+        usu = em.find(Usuari.class, (int) id);
+        hmUsuaris.put(id, usu);
+        
+        return usu;
+    }
+    
+    @Override
+    public void addUsuari(Usuari nouUsuari) throws GestioProjectesException {
+        if (!existeixUsuari(nouUsuari.getId())){
+            em.persist(nouUsuari);
+            hmUsuaris.put(nouUsuari.getId(), nouUsuari);
+        } else {
+            throw new GestioProjectesException("L'usuari que intentes inserir ja existeix");
+        }
+    }
+
+    @Override
+    public void deleteUsuari(int id) throws GestioProjectesException {
+        if (existeixUsuari(id)){
+            em.remove(getUsuari(id));
+            hmUsuaris.remove(id);
+        } else {
+            throw new GestioProjectesException("L'usuari que intentas eliminar no existeix");
+        }
+    }
+
+    @Override
+    public void modificarUsuari(Usuari usuari) throws GestioProjectesException {
+        Usuari usu = getUsuari(usuari.getId());
+        if (existeixUsuari(usuari.getId())){
+            usu.setNom(usuari.getNom());
+            usu.setCognom1(usuari.getCognom1());
+            usu.setCognom2(usuari.getCognom1());
+            usu.setDataNaixement(usuari.getDataNaixement());
+            usu.setLogin(usuari.getLogin());
+            usu.setPasswrdHash(usuari.getPasswrdHash());
+            hmUsuaris.put(usuari.getId(), usuari);
+        } else {
+            throw new GestioProjectesException("L'usuari que intentas modificar no existeix");
+        }
+    }
+
+    @Override
+    public List<Projecte> getLlistaProjectes() throws GestioProjectesException {
+        Query query = em.createQuery("select p from Projecte p", Projecte.class);
+        List<Projecte> projectes = query.getResultList();
+        for (Projecte proj: projectes) {
+            hmProjectes.put(proj.getId(), proj);
+        }
+        return projectes;
+    }
+
+    @Override
+    public Projecte getProjecte(int id) throws GestioProjectesException {
+        if (id <= 0 || id > Integer.MAX_VALUE) {
+            throw new GestioProjectesException("Intent de cercar un projecte amb id " + id + " inadequat");
+        }
+        Projecte proj = hmProjectes.get(id);
+        if (proj != null) {
+            return proj;
+        }
+        
+        proj = em.find(Projecte.class, (int) id);
+        hmProjectes.put(id, proj);
+        
+        return proj;
+    }
+    
+    @Override
+    public List<Projecte> getLlistaProjectesAssignats(Usuari usuari) throws GestioProjectesException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Projecte> getLlistaProjectesNoAssignats(Usuari usuari) throws GestioProjectesException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void assignarProjecte(Usuari usuari, Projecte projecte) throws GestioProjectesException {
+        if (existeixUsuari(usuari.getId()) && existeixProjecte(projecte.getId())){
+            ProjecteUsuariRol pur = new ProjecteUsuariRol(projecte, usuari, getRol(1));
+            em.persist(pur);
+            hmProjectesAssignats.put(projecte.getId(), projecte);
+            hmProjectesNoAssignats.remove(projecte);
+        } else {
+            throw new GestioProjectesException("L'usuari o el projecte no existeixen");
+        }
+    }
+
+    @Override
+    public void desassignarProjecte(Usuari usuari, Projecte projecte) throws GestioProjectesException {
+        if (existeixUsuari(usuari.getId()) && existeixProjecte(projecte.getId())){
+            ProjecteUsuariRol pur = new ProjecteUsuariRol(projecte, usuari, getRol(1));
+            em.remove(pur);
+            hmProjectesAssignats.remove(projecte);
+            hmProjectesNoAssignats.put(projecte.getId(), projecte);
+        } else {
+            throw new GestioProjectesException("L'usuari o el projecte no existeixen");
+        }
+    }
+    
+    @Override
+    public Rol getRol(int id) throws GestioProjectesException {
+        if (id <= 0 || id > Integer.MAX_VALUE) {
+            throw new GestioProjectesException("Intent de cercar un rol amb id " + id + " inadequat");
+        }
+        if (rol != null){
+            return rol;
+        }
+        
+        rol = em.find(Rol.class, (int) id);
+        
+        return rol;
+    }
+    
+    @Override
+    public boolean existeixUsuari(int id) throws GestioProjectesException {
+        if (id <= 0 || id > Integer.MAX_VALUE) {
+            throw new GestioProjectesException("Intent de cercar un usuari amb id " + id + " inadequat");
+        }
+        return em.find(Usuari.class, (int) id) != null;
+    }
+
+    @Override
+    public boolean existeixProjecte(int id) throws GestioProjectesException {
+        if (id <= 0 || id > Integer.MAX_VALUE) {
+            throw new GestioProjectesException("Intent de cercar un projecte amb id " + id + " inadequat");
+        }
+        return em.find(Projecte.class, (int) id) != null;
+    }
+    
     
     @Override
     public void closeCapa() throws GestioProjectesException {
@@ -81,7 +244,7 @@ public class CPJPA implements IGestioProjectes {
             }
         }    
     }
-
+    
     @Override
     public void commit() throws GestioProjectesException {
         try {
@@ -105,6 +268,5 @@ public class CPJPA implements IGestioProjectes {
             throw new GestioProjectesException("Error en fer rollback.", ex);
         }
     }
-    
     
 }
